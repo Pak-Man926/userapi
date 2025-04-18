dac9beeb9093bb79388a0ac9f6598b48869156f8
Pak_Man926 <davidkamau926@gmail.com>
2025-04-18 17:35:04 +0300

    Controller 1.0

--- src/main/java/com/example/userapi/controller/HelloController.java
+++ src/main/java/com/example/userapi/controller/HelloController.java
@@ -1,5 +1,15 @@
 package com.example.userapi.controller;
 
-public class HelloController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hey there, API world 🌍!";
+    }
     
 }

d285b270c9ef071b727aa4c649e1d180d596f7b5
Pak_Man926 <davidkamau926@gmail.com>
2025-04-21 19:46:12 +0300

    Third edit

---
+++ src/main/java/com/example/userapi/models/User.java
@@ -0,0 +1,50 @@
+package com.example.userapi.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String userName;
+    private String password;
+    private String email;
+
+    //Getters and Setters
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+}
